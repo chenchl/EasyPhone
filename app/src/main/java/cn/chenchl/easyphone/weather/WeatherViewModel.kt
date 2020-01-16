68eebb4b4de8003eb5610f2b20e2cc7d78cff98d
ccl <657459942@qq.com>
2020-01-16 16:40:58 +0800

    1.bug fix

--- app/src/main/java/cn/chenchl/easyphone/weather/WeatherViewModel.kt
+++ app/src/main/java/cn/chenchl/easyphone/weather/WeatherViewModel.kt
@@ -120,6 +120,7 @@ class WeatherViewModel : BaseViewModel() {
                                 })
                             }
                         })
+                cityPicker?.show()
             } else {
                 cityPicker!!.show()
             }
